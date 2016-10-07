6661c8c3332408ca46ff3bdc068ece0e499550ff
Seth Kigen <kigen@outlook.com>
2016-10-07 15:27:30 +0300

    Initial Tile notification code

--- BackgroundTaskExample/OurTask/CalculatorTask.cs
+++ BackgroundTaskExample/OurTask/CalculatorTask.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using Windows.ApplicationModel.Background;
+using Windows.Data.Xml.Dom;
 using Windows.Storage;
+using Windows.UI.Notifications;
 
 namespace OurTask
 {
-    public sealed class CalculatorTask: IBackgroundTask
+    public sealed class CalculatorTask : IBackgroundTask
     {
         public void Run(IBackgroundTaskInstance taskInstance)
         {
@@ -12,13 +14,13 @@ namespace OurTask
             var deferral = taskInstance.GetDeferral();
 
             //Generate random numbers.
-            int a = new Random().Next();
-            int b = new Random().Next();
+            var a = new Random().Next(0, 10);
+            var b = new Random().Next(0, 10);
 
             //Sum them.
-            int sum = a + b;
+            var sum = a + b;
 
-            ApplicationDataContainer localSettings
+            var localSettings
                 = ApplicationData.Current.LocalSettings;
 
             //Store the result in ApplicationSettings.
@@ -27,8 +29,36 @@ namespace OurTask
             localSettings.Values["result"] = sum;
 
             localSettings.Values["time"] = DateTime.Now.ToString();
-            
+
+            //Update Tile 
+            UpdateTile();
+
             deferral.Complete();
+        }
+
+        private void UpdateTile()
+        {
+            var tileDocument = new XmlDocument();
+            var xml = @"<tile> " +
+                      "<visual version=\"2\">" +
+                      "<binding template=\"TileSquare150x150PeekImageAndText01\" fallback=\"TileSquarePeekImageAndText01\">" +
+                      "<image id=\"1\" src=\"ms-appx:///Assets/image.JPG\"/>" +
+                      "<text id=\"1\">Row 0</text>" +
+                      " <text id=\"2\">Row 1</text>" +
+                      "<text id=\"3\">Row 2</text>" +
+                      "<text id=\"4\">Row 3</text>" +
+                      " </binding>" +
+                      " </visual>" +
+                      "</tile>";
+
+            tileDocument.LoadXml(xml);
+
+            var tile = new TileNotification(tileDocument);
+
+            var updater = TileUpdateManager
+                .CreateTileUpdaterForApplication();
+
+            updater.Update(tile);
         }
     }
 }
