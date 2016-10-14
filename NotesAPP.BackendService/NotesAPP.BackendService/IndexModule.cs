fe8a1dc1823101d8905cbd81cb6fbb5f17a95d6a
Seth Kigen <kigen@outlook.com>
2016-10-14 15:25:56 +0300

    Final touches on the API

--- NotesAPP.BackendService/NotesAPP.BackendService/IndexModule.cs
+++ NotesAPP.BackendService/NotesAPP.BackendService/IndexModule.cs
@@ -1,4 +1,8 @@
-﻿namespace NotesAPP.BackendService
+﻿using System.Linq;
+using Nancy.ModelBinding;
+using Nancy.Routing;
+
+namespace NotesAPP.BackendService
 {
     using Nancy;
 
@@ -6,10 +10,39 @@
     {
         public IndexModule()
         {
-            Get["/"] = parameters =>
+            NotesDbContext db  = new NotesDbContext();
+            Get["/notes"] = parameters =>
             {
-                return View["index"];
+
+                var notes = db.Notes.AsQueryable();
+                string user = Request.Query["user"];
+
+                if (user != null && !user.Equals(string.Empty))
+                {
+                    notes = notes.Where(n => n.User == user);
+                }
+                return Response.AsJson(notes.ToList());
             };
+
+            Post["/notes"] = parameters =>
+            {
+                var note = this.Bind<Notes>();
+                db.Notes.Add(note);
+                db.SaveChanges();
+                return Response.AsJson(note);
+            };
+
+            Get["/notes/{id}"] = parameters =>
+            {
+                int id = parameters.id;
+                var note = db.Notes.FirstOrDefault(x => x.Id == id);
+                return Response.AsJson(note);
+            };
+
+
+
+            Get["/"] = parameters => Response.AsJson(
+                new {Success = true, Message = "You have arrived..but this backend service is not for humans.."});
         }
     }
 }
